a4529f81103fabd6b863ca823859e3efdc9e0e26
xNickolas <nickolsctt@gmail.com>
2024-02-06 06:14:45 -0300

    Adding style to the form

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,4 +1,10 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGlassCheers, faUsers, faClock } from '@fortawesome/free-solid-svg-icons';
+import lemonOne from '../assets/restaurant.jpg';
+import lemonTwo from '../assets/chef.jpg';
+import lemonThree from '../assets/hero-form.jpg';
+
 
 const BookingForm = (props) => {
     const [date, setDate] = useState("");
@@ -15,13 +21,14 @@ const BookingForm = (props) => {
         setDate(e);
         props.dispatch(e);
     }
+    
 
     return (
         <header>
             <section className='hero'>
                 <div className='hero-form'>
-                    <h2>Little Lemon</h2>
-                    <h3>Chicago</h3>
+                    <h2>Reservations</h2>
+                    <h3>Little Lemon</h3>
                     <p>
                         Reserve your table below, fill in the form with the details of your occasion and we'll send the confirmation to your e-mail address
                     </p>
@@ -30,36 +37,75 @@ const BookingForm = (props) => {
             <section className='form-container'>
                 <form onSubmit={handleSubmit}>
                     <fieldset>
-                        <div>
-                            <label htmlFor='book-date'>Choose Date:</label>
-                            <input id='book-date' value={date} onChange={(e) => handleChange(e.target.value)} type='date' required />
-                        </div>
+                        <div className='form-wrapper'>
+                            <div className='row justify-center'>
+                                <div className='col-lg-6'>
+                                    <div className='form-group'>
+                                        <label htmlFor='book-date' className='form-label'>Choose Date:</label>
+                                        <input id='book-date' className='form-control' value={date} onChange={(e) => handleChange(e.target.value)} type='date' required />
+                                    </div>
 
-                        <div>
-                            <label htmlFor='book-time'>Choose Time:</label>
-                            <select id='book-time' value={times} onChange={(e) => setTimes(e.target.value)}>
-                                <option value="">Select a time</option>
-                                {
-                                    props.availableTimes.availableTimes.map(availableTimes => { return <option key={availableTimes}>{availableTimes}</option> })
-                                }
-                            </select>
-                        </div>
+                                    <div className='form-group'>
+                                        <label htmlFor='book-time' className='form-label'>Choose Time:</label>
+                                        <div className='p-relative'>
+                                            <FontAwesomeIcon icon={faClock} className="occasion-icon" />
+                                            <select id='book-time' className='form-select' value={times} onChange={(e) => setTimes(e.target.value)}>
+                                                <option value="">Select a time</option>
+                                                {
+                                                    props.availableTimes.availableTimes.map(availableTimes => { return <option key={availableTimes}>{availableTimes}</option> })
+                                                }
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
 
-                        <div>
-                            <label htmlFor='book-guests'>Number of Guests:</label>
-                            <input id='book-guests' type="number" placeholder="1" min="1" max="10" value={guests} onChange={(e) => setGuests(e.target.value)} />
-                        </div>
+                                <div className='col-lg-6'>
+                                    <div className='form-group'>
+                                        <label htmlFor='book-guests' className='form-label'>Number of Guests:</label>
+                                        <div className='p-relative'>
+                                            <FontAwesomeIcon icon={faUsers} className="occasion-icon" />
+                                            <select id='book-guests' className='form-select' key={guests} value={guests} onChange={(e) => setGuests(e.target.value)}>
+                                                <option value="">
+                                                    No. of Guests
+                                                </option>
+                                                <option>1 Guest</option>
+                                                <option>2 Guests</option>
+                                                <option>3 Guests</option>
+                                                <option>4 Guests</option>
+                                                <option>5 Guests</option>
+                                                <option>6 Guests</option>
+                                                <option>7 Guests</option>
+                                                <option>8 Guests</option>
+                                                <option>9 Guests</option>
+                                                <option>10 Guests or More</option>
+                                            </select>
+                                        </div>
+                                    </div>
 
-                        <div>
-                            <label htmlFor='book-occasion'>Occasion:</label>
-                            <select id='book-occasion' key={occasion} value={occasion} onChange={e => setOccasion(e.target.value)}>
-                                <option>Birthday</option>
-                                <option>Anniversary</option>
-                            </select>
-                        </div>
+                                    <div className='form-group'>
+                                        <label htmlFor='book-occasion' className='form-label'>Occasion:</label>
+                                        <div className='p-relative'>
+                                            <FontAwesomeIcon icon={faGlassCheers} className="occasion-icon" />
+                                            <select id='book-occasion' className='form-select' key={occasion} value={occasion} onChange={e => setOccasion(e.target.value)}>
+                                                <option value="">Choose your occasion</option>
+                                                <option>Birthday</option>
+                                                <option>Anniversary</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
 
-                        <div className='btnReceive'>
-                            <input aria-label='On Click' type='submit' value={"Make your reservation"} />
+                                <div className='col-12 text-center'>
+                                    <div className='form-images'>
+                                        <img  src={lemonOne}></img>
+                                        <img src={lemonTwo}></img>
+                                        <img  src={lemonThree}></img>
+                                    </div>
+                                    <div className='button-send-reservation'>
+                                        <input aria-label='On Click' type='submit' value={"Make your reservation"} />
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </fieldset>
                 </form>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../assets/Logo.svg';
 import hamburger from '../assets/hamburger-menu.svg';
 import basket from '../assets/basket.svg';
 
 const Nav = () => {
     const [menuOpen, setMenuOpen] = useState(false);
+    const [selectedLink, setSelectedLink] = useState("");
 
     const toggleMenu = () => {
         console.log('toggleMenu called');
         setMenuOpen(!menuOpen);
         console.log('menuOpen state:', menuOpen);
+    }
+
+    const handleLinkClick = (link) => {
+        setSelectedLink(link);
     }
 
     return (
@@ -23,11 +29,54 @@ const Nav = () => {
                     </div>
 
                     <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
-                        <li><a href="/" className='nav-link'>Home</a></li>
-                        <li><a href="#menu" className='nav-link'>Menu</a></li>
-                        <li><a href="#testimonials" className='nav-link'>About</a></li>
-                        <li><a href="/reservations" className='nav-link'>Reservations</a></li>
-                        <li><a href="/order" className='nav-link button-primary order-nav-link'>Order Online</a></li>
+                        <li>
+                            <Link
+                                to="/"
+                                className={`nav-link ${selectedLink === "/" ? "selected" : ""}`}
+                                onClick={() => handleLinkClick("/")}
+                            >
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="#menu"
+                                className={`nav-link ${selectedLink === "#menu" ? "selected" : ""}`}
+                                onClick={() => handleLinkClick("#menu")}
+                            >
+                                Menu
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="#about"
+                                className={`nav-link ${selectedLink === "#about" ? "selected" : ""}`}
+                                onClick={() => handleLinkClick("#about")}
+                            >
+                                About
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link
+                                to="/booking"
+                                className={`nav-link ${selectedLink === "/booking" ? "selected" : ""}`}
+                                onClick={() => handleLinkClick("/booking")}
+                            >
+                                Reservations
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link
+                                to="/order"
+                                className={`nav-link ${selectedLink === "/order" ? "selected" : ""}`}
+                                onClick={() => handleLinkClick("/order")}
+                            >
+                                Order Online
+                            </Link>
+                        </li>
+
                         <li>
                             <a href="/login" className='nav-link nav-basket'>
                                 <img src={basket} alt="Basket" className='icon-basket'></img>
